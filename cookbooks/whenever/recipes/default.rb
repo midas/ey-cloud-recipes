63db951270b86f666b32be66292bddcf673c7f5b
Jason Harrelson <jason@lookforwardenterprises.com>
2012-11-12 12:11:17 -0600

    Add better logging to the whenever recipe.

--- cookbooks/whenever/recipes/default.rb
+++ cookbooks/whenever/recipes/default.rb
@@ -10,18 +10,27 @@ end
 # Set your application name here
 appname = "archivalock"
 
-if ['solo', 'util'].include?(node[:instance_role])
+allowed_roles = %w(solo util)
 
-  # be sure to replace "app_name" with the name of your application.
+if allowed_roles.include?( node[:instance_role] )
+  ey_cloud_report "whenever" do
+    message "executing whenever because instance_role is in #{allowed_roles.inspect}"
+  end
+
   local_user = node[:users].first
+
   execute "whenever" do
     cwd "/data/#{appname}/current"
     user local_user[:username]
     command "bundle exec whenever --update-crontab '#{appname}_#{node[:environment][:framework_env]}'"
     action :run
   end
-  
+else
   ey_cloud_report "whenever" do
-    message "whenever recipe complete"
+    message "NOT executing whenever because instance_role is not in #{allowed_roles.inspect}"
   end
+end
+
+ey_cloud_report "whenever" do
+  message "whenever recipe complete"
 end
